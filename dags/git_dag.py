eb52b901466184b1da9e1a2ab4e39716e1017d0b
vigneshrajan-dev <vigneshrajan93@gmail.com>
2025-10-16 15:42:07 +0530

    Initial commit for Astro deployment

--- dags/git_dag.py
+++ dags/git_dag.py
@@ -18,15 +18,15 @@ start_date=days_ago(1),
 catchup=False,
 ) as dag:
 
-hello_task = BashOperator(
-task_id='print_hello',
-bash_command='echo "Hello from Astro Cloud deployment!"',
-)
+    hello_task = BashOperator(
+    task_id='print_hello',
+    bash_command='echo "Hello from Astro Cloud deployment!"',
+    )
 
-goodbye_task = BashOperator(
+    goodbye_task = BashOperator(
 
-task_id='print_goodbye',
-bash_command='echo "Goodbye from Astro Cloud deployment!"',
-)
+    task_id='print_goodbye',
+    bash_command='echo "Goodbye from Astro Cloud deployment!"',
+    )
 
-hello_task >> goodbye_task
+hello_task >> goodbye_taskgit 

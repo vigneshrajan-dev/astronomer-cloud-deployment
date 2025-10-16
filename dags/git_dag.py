f7ff2f5089c888d85581af8f2e6c90bac1449182
vigneshrajan-dev <vigneshrajan93@gmail.com>
2025-10-16 16:15:42 +0530

    changes to git dag

--- dags/git_dag.py
+++ dags/git_dag.py
@@ -1,32 +1,31 @@
 from airflow import DAG
-
 from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
+from datetime import timedelta  # <-- Add this import
 
 default_args = {
-'owner': 'airflow',
-'retries': 1,
-'retry_delay': timedelta(minutes=5),
+    'owner': 'airflow',
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
 }
 
 with DAG(
-'git_deployment_example',
-default_args=default_args,
-description='A simple DAG deployed via GitHub integration',
-schedule_interval='@daily',
-start_date=days_ago(1),
-catchup=False,
+    'git_deployment_example',
+    default_args=default_args,
+    description='A simple DAG deployed via GitHub integration',
+    schedule_interval='@daily',
+    start_date=days_ago(1),
+    catchup=False,
 ) as dag:
 
     hello_task = BashOperator(
-    task_id='print_hello',
-    bash_command='echo "Hello from Astro Cloud deployment!"',
+        task_id='print_hello',
+        bash_command='echo "Hello from Astro Cloud deployment!"',
     )
 
     goodbye_task = BashOperator(
-
-    task_id='print_goodbye',
-    bash_command='echo "Goodbye from Astro Cloud deployment!"',
+        task_id='print_goodbye',
+        bash_command='echo "Goodbye from Astro Cloud deployment!"',
     )
 
-hello_task >> goodbye_taskgit 
+    hello_task >> goodbye_task  # <-- Fix typo here
